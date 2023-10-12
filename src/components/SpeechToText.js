331f17091bf6cee2dec37f6e2d64569eeb93de89
Rupal <rupalgupta951@gmail.com>
2023-10-12 17:24:11 +0530

    changes the UI theme

--- src/components/SpeechToText.js
+++ src/components/SpeechToText.js
@@ -28,9 +28,9 @@ function SpeechToText() {
           Start Listening
         </button>
         <button className="btn" onClick={() => SpeechRecognition.stopListening()}>
-          stop Listening
+          Stop Listening
         </button>
-        <button className="btn" onClick={resetTranscript}>Clear text</button>
+        <button className="btn" onClick={resetTranscript}>Clear Text</button>
         <button className="btn" onClick={setCopied}>{isCopied ? "Copied! 👍" : "Copy?"}</button>
       </div>
     </div>
